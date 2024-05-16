3897aa3b8fbee55473ad9050b16f18d373d08def
rois.alhaqq <rois.alhaqq@Muhammad-Rois-Alhaqq.local>
2024-05-16 22:25:47 +0700

    add patient

--- internal/db/migrations/20240515223535_init_table_patients.up.sql
+++ internal/db/migrations/20240515223535_init_table_patients.up.sql
@@ -1,2 +1,23 @@
-DROP TABLE IF EXISTS product CASCADE;
-DROP TYPE IF EXISTS category_product_name CASCADE;
+CREATE TYPE patient_gender AS ENUM('female', 'male');
+
+CREATE TABLE patients IF NOT EXISTS(
+  identity_number varchar(16) primary key,
+  name varchar(30) not null,
+  birth_date date not null,
+  phone_number varchar(15) not null,
+  gender patient_gender not null,
+  card_image varchar(255) not null,
+  created_at timestamp
+);
+
+
+CREATE INDEX IF NOT EXISTS patients_identity_number
+	ON patients(identity_number);
+CREATE INDEX IF NOT EXISTS patients_name
+	ON patients(lower(name));
+CREATE INDEX IF NOT EXISTS patients_phone_number
+	ON patients(phone_number);
+CREATE INDEX IF NOT EXISTS patients_created_at_desc
+	ON patients(created_at desc);
+CREATE INDEX IF NOT EXISTS patients_created_at_asc
+	ON patients(created_at asc);

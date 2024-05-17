0a3d2424bc3be4dccee164147f20d539c495545c
andrt <alifandarta26@gmail.com>
2024-05-17 17:16:38 +0700

    feat(patient): create patient

--- internal/db/model/patient.go
+++ internal/db/model/patient.go
@@ -1,1 +1,22 @@
 package model
+
+import "time"
+
+type PatientGender string
+
+const (
+	Male   PatientGender = "male"
+	Female PatientGender = "female"
+)
+
+var Genders = []interface{}{Male, Female}
+
+type Patient struct {
+	IdentityNumber string        `db:"user_id"`
+	Name           string        `db:"name"`
+	BirthDate      string        `db:"birth_date"`
+	PhoneNumber    string        `db:"phone_number"`
+	CardImage      string        `db:"card_image"`
+	Gender         PatientGender `db:"gender"`
+	CreatedAt      time.Time     `db:"created_at"`
+}

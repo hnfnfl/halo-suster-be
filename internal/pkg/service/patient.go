1478eab96beeebe7e81a03e2f5d551948b4faaa4
andrt <alifandarta26@gmail.com>
2024-05-18 11:14:21 +0700

    refactor(patient) : remove transaction query

--- internal/pkg/service/patient.go
+++ internal/pkg/service/patient.go
@@ -13,18 +13,8 @@ import (
 func (s *Service) Create(patient model.Patient) errs.Response {
 	var err error
 
-	tx, err := s.DB().Begin()
-	if err != nil {
-		return errs.NewInternalError("transaction error", err)
-	}
-	defer func() {
-		if err := tx.Rollback(); err != nil {
-			return
-		}
-	}()
-
 	stmt := "INSERT INTO patients (identity_number,name,birth_date, phone_number,gender,card_image,created_at) VALUES ($1, $2, $3, $4, $5, $6, $7)"
-	if _, err = tx.Exec(stmt, patient.IdentityNumber, patient.Name, patient.BirthDate, patient.PhoneNumber, patient.Gender, patient.CardImage, time.Now()); err != nil {
+	if _, err = s.DB().Exec(stmt, patient.IdentityNumber, patient.Name, patient.BirthDate, patient.PhoneNumber, patient.Gender, patient.CardImage, time.Now()); err != nil {
 		return errs.NewInternalError("insert error", err)
 	}
 	return errs.Response{
